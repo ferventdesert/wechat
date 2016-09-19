68373914853ffc3112defc73267c31f43d8acf37
赵一鸣 <desert.zym@alipay.com>
2016-09-19 20:49:11 +0800

    改进格式

--- app.py
+++ app.py
@@ -100,20 +100,28 @@ def get_yaohao(id):
         dic = dict(zip( columns,res))
         #dic={'count': 31, 'end': 49, 'lost': [25, 26, 27, 34, 35, 50, 51], 'selected': 0, 'period': 26, 'start': 19, 'id': '5606101836469'}
     #print dic;
+    start= int(dic['start'])
+    end=int(dic['end'])
+    period= int(dic['period']);
     dic['start']= period_dict[dic['start']];
     dic['select']= u'恭喜摇中!摇中了还查什么...' if dic['selected']==1 else u'还没有摇中/(ㄒoㄒ)/'
-    dic['lost_count']=len(dic['lost'].split(' '));
-    dic['lost']= u'【' + ' '.join( period_dict[get_int(r)] for r in  dic['lost'].split(' ') if r.strip()!='')+u'】';
+    lost_count=end-start+1-period
+    if lost_count==0:
+        dic['lost']=u'您没有拉下一次摇号!'
+    else:
+        lost_period=u'【' + ' '.join( period_dict[get_int(r)] for r in  dic['lost'].split(' ') if r.strip()!='')+u'】';
+        dic['lost']= u'期间中断了{lost_count}期,分别是{lost}'.format(lost_count=lost_count,lost=lost_period);
+
     dic['ratio']= get_int(get_int(dic['count'])/6.0);
     dic['percent']= round(dic['count']/6.0/991.0,5);
-    res= u'您的编号{id}\n在{start}期开始摇号,总计{count}次,{select}\n期间中断了{lost_count}期,分别是{lost} \n目前中签倍率为{ratio}倍, 下期摇中概率{percent}'.format(**dic);
+    res= u'您的编号{id}\n在{start}期开始摇号,总计{count}次,{select}\n{lost}\n目前中签倍率为{ratio}倍, 下期摇中概率{percent}'.format(**dic);
     return res;
 
 if __name__ == '__main__':
     # print get_response(u'历史')
     # print get_response(u'帮助')
     # print get_response(u'转载')
-    print get_response(u'1193432842389423732')
+    print get_response(u'5935104232928')
     print get_response(u'5606101836469')
     #exit()
     app.run(host='0.0.0.0', port=80, debug=False)
